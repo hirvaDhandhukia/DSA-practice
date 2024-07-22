d5155d3e71dcbf7bb1eacae3879f63b3560f95bd
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2024-07-22 22:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (67.98%) - LeetSync

--- leetcode/88-merge-sorted-array/merge-sorted-array.java
+++ leetcode/88-merge-sorted-array/merge-sorted-array.java
@@ -1,29 +1,20 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // catch: stored inside the array nums1
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        int r1 = m-1;
+        int r2 = arr2.length -1;
+        int w = m+n-1;
 
-
-
-        int[] temp = new int[m];
-        int i=0,j=0,k = 0;
-
-        while(k<m) {
-            temp[k] = nums1[k++];
-        }
-        k=0;
-        while(k<m && j<n) {
-            if(temp[k] <= nums2[j]) {
-                nums1[i++] = temp[k++];
+        while(w >=0) {
+            if(r1 >= 0 && r2 >= 0) {
+                // we'll input whatever is bigger
+                arr1[w] = arr1[r1] > arr2[r2] ? arr1[r1--] : arr2[r2--];
+            } else if (r1>=0){
+                arr1[w] = arr1[r1--];
             } else {
-                nums1[i++] = nums2[j++];
+                arr1[w] = arr2[r2--];
             }
+            w--;
         }
 
-        while(k<m) {
-            nums1[i++] = temp[k++];
-        }
-        while(j<n) {
-            nums1[i++] = nums2[j++];
-        }
     }
 }
