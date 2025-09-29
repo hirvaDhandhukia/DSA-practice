a72217749c13c072a432e0663716d80765b16217
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2025-09-29 01:01:47 -0300

    Time: 2 ms (78.87%) | Memory: 46.2 MB (55.59%) - LeetSync

--- leetcode/283-move-zeroes/move-zeroes.java
+++ leetcode/283-move-zeroes/move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int position = 0;
+        int p=0;
         for(int i=0; i< nums.length; i++) {
-            if(nums[i] != 0 ) {
-                nums[position++] = nums[i];
-            }
+            if(nums[i] != 0) {
+                nums[p] = nums[i];
+                p++;
+            } 
         }
-        for(int i=position; i<nums.length; i++) {
+        for(int i=p; i<nums.length; i++) {
             nums[i] = 0;
         }
     }
