5743215f78f15b09fbdad0b9aca8870f97734ce5
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2025-11-10 00:34:58 -0400

    Time: 0 ms (100.00%) | Memory: 44.5 MB (5.33%) - LeetSync

--- leetcode/1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ leetcode/1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -2,14 +2,6 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] runningSum(int[] nums) {
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // int curr_sum = nums[1];
-        // for(int i=1; i<=nums.length; i++) {
-        //     curr_sum = (i*(i+1))/2;
-        //     arr.add(curr_sum);
-        // }
-        // return arr.stream().mapToInt(Integer::intValue).toArray();
-
         for(int i=1; i<nums.length; i++) {
             nums[i] = nums[i] + nums[i-1];
         }
