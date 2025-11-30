f40d982b1dd01837e24534d79b215e1182d58c55
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2025-11-30 16:44:29 -0400

    Time: 1 ms (99.83%) | Memory: 55.7 MB (11.39%) - LeetSync

--- leetcode/169-majority-element/majority-element.java
+++ leetcode/169-majority-element/majority-element.java
@@ -1,21 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    // Boyer-moore voting algorithm
-        int ans = nums[0], vote = 1;
-        for(int i=0; i< nums.length; i++) {
+        // a majority varibale & vote 
+        int major = nums[0], vote = 0;
+
+        // traversing loop and updating vote 
+        for(int i=0; i<nums.length; i++) {
             if(vote == 0) {
-                ans = nums[i];
-                vote++;
-            } else if (ans == nums[i]) {
-                vote++;
-            } else {
-                vote--; 
-                if (vote == 0) {
-                    ans = nums[i];
-                    vote = 1;
-                }
+                major = nums[i];
+                vote = 1;
+            }
+            else {
+                if(major == nums[i]) {
+                    vote++;
+                } else vote--;
             }
         }
-        return ans; 
+
+        return major;
     }
 }
