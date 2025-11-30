4a0dce05488ff0473c02d7b671f31b9e65bf0958
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2025-11-29 20:25:11 -0400

    Time: 1 ms (94.66%) | Memory: 47.5 MB (14.49%) - LeetSync

--- leetcode/724-find-pivot-index/find-pivot-index.java
+++ leetcode/724-find-pivot-index/find-pivot-index.java
@@ -1,16 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int suml = 0, sumr;
-        int totalsum = 0;
+        int total = 0;
+        for(int n : nums) total += n; 
+
+        int leftsum = 0;
+
         for(int i=0; i<nums.length; i++) {
-            totalsum += nums[i];
-        }
-        for(int i=0; i<nums.length; i++) {
-            sumr = totalsum - suml - nums[i];
-            if(suml == sumr) {
-                return i;
-            }
-            suml += nums[i];
+            // go through each item in a loop & calculate individual right sum
+            int rightsum = total - leftsum - nums[i];
+            
+            // checking pivot index criteria
+            if(leftsum == rightsum) return i;
+
+            // update new leftsyum for the next loop
+            leftsum = leftsum + nums[i];
         }
         return -1;
     }
