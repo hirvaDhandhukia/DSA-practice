9d80fbfeccc1163a429d21afe87295cdd6be38b7
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2026-03-05 16:19:47 -0400

    Time: 427 ms (94.49%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/584-find-customer-referee/find-customer-referee.sql
+++ leetcode/584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,7 @@
-select name from Customer 
-where not referee_id = 2 or referee_id is null
+-- select name from Customer 
+-- where not referee_id = 2 or referee_id is null
 
 -- can't use '=' with null condition
+
+select name from Customer 
+where referee_id != 2 or referee_id is null
